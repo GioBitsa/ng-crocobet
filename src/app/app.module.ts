1ad5a468c4dd1b5c84d98555ac227884e65fe691
giobitsa <giorgi.bitsadze01@gmail.com>
2023-05-28 21:16:10 +0400

    working on navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,17 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { HomeComponent } from './pages/home/home.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, SidebarComponent],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    SidebarComponent,
+    MainLayoutComponent,
+    NavbarComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
